a2efdc3c42c264239c78bffcc5c52935bce1621b
jwchong93 <jwchong93@gmail.com>
2014-04-04 14:42:15 +0800

    Added the function evaluateAllOperationsOnStack().

--- src/Calculator.c
+++ src/Calculator.c
@@ -4,8 +4,49 @@
 #include <malloc.h>
 #include "CException.h"
 #include "Error.h"
+
+
+void evaluateAllOperatorsOnStack(Stack *operatorStack,Stack *dataStack)
+{
+	OperatorToken *newToken =(OperatorToken*) pop(operatorStack);
+	while(newToken!=NULL)
+	{
+		evaluateOperator(dataStack,newToken);
+		newToken =(OperatorToken*) pop(operatorStack);
+	}
+}
+
 void tryEvaluateOperatorOnStackThenPush(Stack *operatorStack,Stack *dataStack,OperatorToken *operator)
 {
+	OperatorToken *newToken;
+	newToken = (OperatorToken*)pop(operatorStack);
+	if(newToken == NULL)
+	{
+		push(operatorStack,operator);
+	}
+	else
+	{
+				while(newToken!=NULL)
+				{
+					if(operator->precedence>newToken->precedence)
+					{
+						break;
+					}
+					else
+					{
+						evaluateOperator(dataStack,newToken);
+					}
+					newToken = (OperatorToken*)pop(operatorStack);
+				}
+				if(newToken!=NULL)
+				{
+				
+				push(operatorStack,newToken);
+				}
+				push(operatorStack,operator);
+			
+		
+	}
 	
 }
 
@@ -65,6 +106,7 @@ void evaluateOperator(Stack *dataStack,OperatorToken *operator)
 		}
 		
 	}
+
 	answer = createNumberToken(result);
 	push(dataStack,answer);
 	free(num1);
