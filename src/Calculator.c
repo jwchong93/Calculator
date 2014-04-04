c1c8b56cfcc35894c437ad5b89426c32772d39e6
jwchong93 <jwchong93@gmail.com>
2014-04-04 13:07:24 +0800

    Added the fucntion evaluateOperator().

--- src/Calculator.c
+++ src/Calculator.c
@@ -1,1 +1,72 @@
 #include "Calculator.h"
+#include "Stack.h"
+#include "Token.h"
+#include <malloc.h>
+#include "CException.h"
+#include "Error.h"
+void tryEvaluateOperatorOnStackThenPush(Stack *operatorStack,Stack *dataStack,OperatorToken *operator)
+{
+	
+}
+
+void evaluateOperator(Stack *dataStack,OperatorToken *operator)
+{
+	NumberToken *num1;
+	NumberToken *num2;
+	NumberToken *answer;
+	int result;
+	num1 = (NumberToken *)pop(dataStack);
+	num2 = (NumberToken *)pop(dataStack);
+	switch(operator->name[0])
+	{
+		case '+':
+		{
+			result = num1->value+num2->value;
+			break;
+		}
+		case '-':
+		{
+			result = num1->value-num2->value;
+			break;
+		}
+		case '*':
+		{
+			result = num1->value*num2->value;
+			break;
+		}
+		case '/':
+		{
+			result = num1->value/num2->value;
+			break;
+		}
+		case '%':
+		{
+			result = num1->value%num2->value;
+			break;
+		}
+		case '^':
+		{
+			result = num1->value^num2->value;
+			break;
+		}
+		case '&':
+		{
+			result = num1->value&num2->value;
+			break;
+		}
+		case '|':
+		{
+			result = num1->value|num2->value;
+			break;
+		}
+		default:
+		{
+			Throw(ERR_NOT_OPERATOR);
+		}
+		
+	}
+	answer = createNumberToken(result);
+	push(dataStack,answer);
+	free(num1);
+	free(num2);
+}
--- src/Calculator.h
+++ src/Calculator.h
@@ -1,5 +1,11 @@
 #ifndef Calculator_H
 #define Calculator_H
+#include "Stack.h"
+#include "Token.h"
 
+void tryEvaluateOperatorOnStackThenPush(Stack *operatorStack,Stack *dataStack,OperatorToken *operator);
+void evaluateAllOperatorsOnStack(Stack *operatorStack,Stack *dataStack);
+void evaluateOperator(Stack *dataStack,OperatorToken *operator);
+int evaluate(char *expression);
 
 #endif // Calculator_H
